d2cfc206970c8d2e14d0929c97721d8c9aa99f8f
franciscoCorrea <franciscobmbc@gmail.com>
2023-04-20 04:27:47 +0100

    main menu layout ready

--- menu.js
+++ menu.js
@@ -3,4 +3,12 @@ function draw() {
     if (canvas.getContext) {
         const ctx = canvas.getContext("2d");
     }
+
+    const hideDivsBtn = document.querySelector('#hideDivsBtn');
+const divsToHide = document.querySelectorAll('.box');
+hideDivsBtn.addEventListener('click', function() {
+  for (let i = 0; i < divsToHide.length; i++) {
+    divsToHide[i].style.display = 'none';
+  }
+});
 }
